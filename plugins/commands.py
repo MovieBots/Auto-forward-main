a218c6a001ebd106ab11f9e20980e2a432e849b2
MovieBots <154045205+MovieBots@users.noreply.github.com>
2026-07-25 23:57:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -138,7 +138,7 @@ async def start_forward(bot, userid, source_chat_id, last_msg_id):
             async for msg in bot.iter_messages(source_chat_id, int(last_msg_id), int(temp_utils.CURRENT)):
                 file_caption = re.sub(r"(❤️‍🔥 Join ~ [ @Moonknight_media ])|(🤖 Join Us [@BoB_Files1])|(𝗧𝗛𝗘 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 )|(𝙿𝚘𝚠𝚎𝚛e𝚍 𝙱𝚢 ➥  @Theprofessers)|(❤️‍🔥 Join ~ [ @Moonknight_media ])|(\n🔸 Upload By \[@BlackDeath_0\])|(\n❤️‍🔥 Join ~ \[@Moonknight_media\])|(@Ac_Linkzz)|(\n⚡️Join:- \[@BlackDeath_0\]‌‌)|(EonMovies)|(\nJOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+)", " ", str(msg.caption))
                 file_caption = f"""{file_caption}\n➖➖➖➖➖➖➖➖➖➖
- <b>Powered By:</b> <a href='https://t.me/MovieTimesTV'><b>Mᴏᴠɪᴇ Tɪᴍᴇs™</b></a>
+ <b>Powered By:</b> <a href='https://t.me/Film_CityOfficial'><b>Fɪʟᴍ Cɪᴛʏ Oғғɪᴄɪᴀʟ</b></a>
 ➖➖➖➖➖➖➖➖➖➖"""
                 if temp_utils.CANCEL:
                     status = 'Cancelled !'
